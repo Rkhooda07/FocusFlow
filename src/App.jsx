7b0a0c4f38cbca4abfdf5c719589fe1eaa768612
Rakshit Hooda <rakshit.official703@gmail.com>
2026-02-07 23:35:49 +0530

    setup routing and base pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Tasks'
+import Focus from './pages/Focus'
+import Habits from './pages/Habits'
+import Settings from './pages/Settings'
 
 function App() {
 
   return (
-    <>
-      <p>Welcome to FocusFlow</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Dashboard />} />
+        <Route path="/tasks"  element= {<Tasks />}/>
+        <Route path="/focus" element= {<Focus />}/>
+        <Route path="/habits" element= {<Habits />}/>
+        <Route path="/settings" element= {<Settings />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
